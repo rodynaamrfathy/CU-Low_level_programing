a9b5f2b534227e972713fa2e475b21f078c1ce12
rodynaamrfathy <rodynaamr@icloud.com>
2024-02-28 22:01:29 +0200

    ‘UPDATE’

---
+++ sorting_algorithms/selection_sorting.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void selection_sort(int *arr, size_t size) {
+    int temp, min;
+    for (size_t start = 0; start < size - 1; start++) {
+        min = start;
+        for (size_t index = start + 1; index < size; index++) {
+            if (arr[min] > arr[index]) {
+                min = index;
+            }
+        }
+        temp = arr[start];
+        arr[start] = arr[min];
+        arr[min] = temp;
+    }
+}
+
+int main() {
+    int arr1[] = {43, 27, 98, 65, 12};
+    size_t size = sizeof(arr1) / sizeof(arr1[0]);
+
+    cout << "Before Sorting:" << endl;
+    for (size_t i = 0; i < size; i++) {
+        cout << arr1[i] << "\t";
+    }
+
+    selection_sort(arr1, size);
+
+    cout << "\nAfter Sorting:" << endl;
+    for (size_t i = 0; i < size; i++) {
+        cout << arr1[i] << "\t";
+    }
+
+    return 0;
+}
